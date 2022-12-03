e14243504c052e3e98ad0fb27bbec5f4bcb0071c
taiwo <X00167646@myTUDublin.ie>
2022-12-03 12:24:57 +0000

    Button for submission

--- TestApi/WeatherApiModel.cs
+++ TestApi/WeatherApiModel.cs
@@ -7,8 +7,27 @@ public class WeatherApiModel
     
     [Required]
     [StringLength(15, ErrorMessage = "Country name too long.")]
-    public String? Country { get; set; }
-    //private String[] countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Antigua and Barbuda","Argentina","Armenia","Austria","Azerbaijan","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Brazil","Brunei","Bulgaria","Burkina Faso","Burundi","Cabo Verde","Cambodia","Cameroon","Canada","Central African Republic","Chad","Channel Islands","Chile","China","Colombia","Comoros","Congo","Costa Rica","Cote d'Ivoire","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","DR Congo","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Eswatini","Ethiopia","Faeroe Islands","Finland","France","French Guiana","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Grenada","Guatemala","Guinea","Guinea-Bissau","Guyana","Haiti","Holy See","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macao","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mayotte","Mexico","Moldova","Monaco","Mongolia","Montenegro","Morocco","Mozambique","Myanmar","Namibia","Nepal","Netherlands","Nicaragua","Niger","Nigeria","North Korea","North Macedonia","Norway","Oman","Pakistan","Panama","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Reunion","Romania","Russia","Rwanda","Saint Helena","Saint Kitts and Nevis","Saint Lucia","Saint Vincent and the Grenadines","San Marino","Sao Tome & Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","State of Palestine","Sudan","Suriname","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","The Bahamas","Timor-Leste","Togo","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Venezuela","Vietnam","Western Sahara","Yemen","Zambia","Zimbabwe"];
+    public string? PlaceName { get; set; }
+    public string? countryName { get; set; }
+    public string? regionName { get; set; }
+    public string? temperature { get; set; }
+    public string? locationName { get; set;  }
+    public string? conditionIcon { get; set; }
+    public string? conditionText { get; set; }
+    public string? windMph { get; set; }
+    public string? windDegree { get; set; }
+    public string? windDirection { get; set; }
+    public string? pressure { get; set; }
+    public string? precipitation { get; set; }
+    public string? humidity { get; set; }
+    public string? feelsLike { get; set; }
+    public string? uvIndex { get; set; }
+    public string? gust { get; set; }
+    public string? localTimeFromApi { get; set; }
+    public string? localTimeFromApiFormatted { get; set; }
+    
+    public String name;
+    
     
     
     
